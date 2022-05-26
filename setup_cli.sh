a019a4036d46c1d7ef54c5b921394d8eb2148591
davide_elliot <salvadore.davide@gmail.com>
2022-05-26 16:35:22 +0200

    Setup

---
+++ setup_cli.sh
@@ -0,0 +1,12 @@
+set -e
+set -x
+
+
+sudo apt-get install -y python3
+sudo apt-get install pip3
+sudo pip3 install typer
+
+sudo chmod +x fopt
+
+echo 'export PATH=$HOME/faas-optimizer/cli:$PATH' > "$HOME/.bash_profile"
+
