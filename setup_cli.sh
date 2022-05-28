b9dd46811ce607fa2c63cd835e64be3aaf9222ad
davide_elliot <salvadore.davide@gmail.com>
2022-05-28 17:25:11 +0200

    Fixed wrong commands

--- setup_cli.sh
+++ setup_cli.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-set -e
-set -x
-
 
 sudo apt-get install -y python3
-sudo apt-get install pip3
+sudo apt install python3-pip
 sudo pip3 install typer
 
 sudo chmod +x fopt
 
-echo 'export PATH=$HOME/faas-optimizer/cli:$PATH' > "$HOME/.bash_profile"
+
+echo '\n\e[32mPlease add the following lines to your .bashrc or your .bash_profile file:\e[0m\n' 
+echo '\t\e[34mexport PATH=$HOME/faas-optimizer/cli:$PATH\n\texport FOPT_API_HOST=<FAAS-OPTIMIZER-URI>:<FAAS-OPTIMIZER-PORT>\e[0m\n'
+echo '\e[32mThen run:\e[0m\n'
+echo '\t\e[31msource $HOME/.bash_profile or source $HOME/.bashrc\e[0m\n\n'
 
